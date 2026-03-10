d9a9fdca5a0f27e544c2b1c242c6fb724973eb20
kevin <kevin.kafg@gmail.com>
2026-03-10 13:11:18 +0100

    añadido buscador en javascript y un service para que funcione, correcciones varias

--- src/main/java/com/alquilatusvehiculos/alquila_tus_vehiculos/controller/SucursalController.java
+++ src/main/java/com/alquilatusvehiculos/alquila_tus_vehiculos/controller/SucursalController.java
@@ -1,11 +1,10 @@
 package com.alquilatusvehiculos.alquila_tus_vehiculos.controller;
 
 import com.alquilatusvehiculos.alquila_tus_vehiculos.model.Sucursal;
-import com.alquilatusvehiculos.alquila_tus_vehiculos.repository.SucursalRepository;
+import com.alquilatusvehiculos.alquila_tus_vehiculos.service.SucursalService;
 import lombok.RequiredArgsConstructor;
-import java.util.List;
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -13,37 +12,24 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class SucursalController {
 
-    private final SucursalRepository sucursalRepository;
+    private final SucursalService sucursalService;
 
-    @GetMapping("/lista")
-    public String mostrarSucursales(Model model){
-        List<Sucursal> listaSucursal = sucursalRepository.findAll();
-        model.addAttribute("listaSucursal", listaSucursal);
-        return "sucursales/lista";
-    }
-
-    @GetMapping("/nueva")
-    public String nuevaSucursal(Model model){
-        model.addAttribute("sucursal", new Sucursal());
-        return "sucursales/formulario";
-    }
-
-    @GetMapping("/editar/{id}")
-    public String editarFormulario(@PathVariable Long id, Model model) { // Cambiado 'long' por 'Long'
-        Sucursal sucursalExistente = sucursalRepository.findById(id).orElse(null);
-        model.addAttribute("sucursal", sucursalExistente);
-        return "sucursales/formulario";
+    @GetMapping
+    public String mostrarSucursales(Model model) {
+        model.addAttribute("listaSucursal", sucursalService.obtenerTodas());
+        model.addAttribute("nuevaSucursal", new Sucursal());
+        return "sucursales/admin";
     }
 
     @PostMapping("/guardar")
-    public String guardarSucursal(@ModelAttribute Sucursal sucursal){
-        sucursalRepository.save(sucursal);
-        return "redirect:/sucursales/lista";
+    public String guardar(@ModelAttribute Sucursal sucursal) {
+        sucursalService.guardar(sucursal);
+        return "redirect:/sucursales";
     }
 
     @GetMapping("/borrar/{id}")
-    public String borrarSucursal(@PathVariable Long id) {
-        sucursalRepository.deleteById(id);
-        return "redirect:/sucursales/lista";
+    public String borrar(@PathVariable Long id) {
+        sucursalService.borrar(id);
+        return "redirect:/sucursales";
     }
 }
