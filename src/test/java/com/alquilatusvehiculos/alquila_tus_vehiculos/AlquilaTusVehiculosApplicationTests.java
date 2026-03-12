fd90209dc58e2f2761a92068d8d50e524099c995
Mar <marbou96@hotmail.com>
2026-03-12 12:02:18 +0100

    modificando ci.yml añadiendo los tests ahora que tenemos base de datos

---
+++ src/test/java/com/alquilatusvehiculos/alquila_tus_vehiculos/AlquilaTusVehiculosApplicationTests.java
@@ -0,0 +1,17 @@
+package com.alquilatusvehiculos.alquila_tus_vehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilaTusVehiculosApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Este método está vacío a propósito.
+        // Evalúa si el "Contexto de Spring" puede cargarse correctamente.
+        // Si hay un error en una entidad, un fallo en un controlador,
+        // o la base de datos no conecta, este test fallará y protegerá tu código.
+    }
+
+}
