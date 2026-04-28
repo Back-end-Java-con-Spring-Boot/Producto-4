a8e90a10d9256bf368a5f1945853e3edbf7087c2
kevin <kevin.kafg@gmail.com>
2026-04-29 01:01:38 +0200

    implementacion de arquitectura de seguridad híbrida

---
+++ src/main/java/com/alquilatusvehiculos/alquila_tus_vehiculos/security/JwtUtils.java
@@ -0,0 +1,52 @@
+package com.alquilatusvehiculos.alquila_tus_vehiculos.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private final String JWT_SECRET = "Sleipnir_Super_Secret_Key_2026_NoobDevs_Security_Master_Key";
+
+    private final int JWT_EXPIRATION_MS = 86400000;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
+    }
+
+    public String generateJwtToken(Authentication authentication) {
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + JWT_EXPIRATION_MS))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(authToken);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            System.out.println("Error en el Token: " + e.getMessage());
+        }
+        return false;
+    }
+}
